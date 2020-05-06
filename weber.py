3b18ac70c5a281ca474b2a7b0b8e61d9120dac13
NeilSCGH <neil.segard@gmail.com>
2020-05-06 16:16:10 +0200

    new code

--- weber.py
+++ weber.py
@@ -27,7 +27,7 @@ class program():
         if urlparse(self.url).scheme == "": self.url = "http://" + self.url
 
         self.data=urlparse(self.url)
-        self.mainDomain=self.getMainDomain(self.url)
+        
 
         self.deep = int(self.tool.tryToGetValue("-deep",2))
         self.outputFileName = self.tool.tryToGetValue("-o","")
@@ -37,165 +37,109 @@ class program():
 
         self.otherDomains=[]
 
+
+
+        self.urlsToScan=[self.url]
+        self.urlsFound=[self.url]
+
     def run(self):
         print("Starting scan of {} with deep {}".format(self.url,self.deep))
-        listToScan=[self.url]
-        listScanned=[]
 
         for i in range(self.deep):
-            print("\nDeep {}/{}".format(i+1,self.deep))
-            sys.stdout.flush()
+            print("\nDeep {}/{}".format(i+1, self.deep))
 
-            newListToScan=[]
-            for url in listToScan:
-                if self.verbose: 
-                    print("SCANNING {}".format(urlparse(url).path))
-                else:
-                    print(".",end="")
-                sys.stdout.flush()
-                newListToScan += self.extract(url)
-                listScanned.append(self.removeSchemeUrl(url))
+            newUrls = self.scan()#will scan self.urlsToScan
 
-            for url in listScanned:
-                while ("http://"+url) in newListToScan:
-                    newListToScan.remove("http://"+url)
-                while ("https://"+url) in newListToScan:
-                    newListToScan.remove("https://"+url)
+            newUrls = self.removeDuplicates(newUrls)
+            newUrls = self.filter(newUrls)
 
-            listToScan=list(dict.fromkeys(newListToScan)) #removing duplicates
-            if not self.verbose: print("")
-            print("{} new urls found".format(len(listToScan)))
+            self.urlsToScan = newUrls[:]
+            self.urlsFound += newUrls
 
-            if len(listToScan)==0: break
+        print("\n{} urls found but not scanned".format(len(self.urlsToScan)))
+
+        print("\nUrls found are:")
+        self.printList(self.urlsFound)
+
+    def printList(self,tab):
+        tab.sort()
+        for elem in tab:
+            print(elem)
         
-        nbToScan=len(listToScan)
-        if nbToScan!=0: print("\n{} Urls not scanned!".format(nbToScan),end='')
+    def removeDuplicates(self,tab):
+        return list(dict.fromkeys(tab))
 
-        listToSave=listScanned[:]
-        for i in range(len(listToScan)):
-            listToSave.append(self.removeSchemeUrl(listToScan[i]))
-        listToSave.sort()
+    def filter(self, tab):
+        newTab=self.filterDomain(tab)
+        newTab=self.filterAlreadyFound(newTab)
+        return newTab
 
-        self.otherDomains= self.removeDuplicates(self.otherDomains)
-        self.otherDomains.sort()
+    def filterDomain(self, tab):
+        ourDomain = self.getDomain(self.url)
+        newTab=[]
+        for url in tab:
+            if self.getDomain(url) == ourDomain:
+                newTab.append(url)
+        return newTab
 
-        if self.verbose: 
-            self.printFoundUrls(listToSave)
-            self.printOtherDomains()
+    def filterAlreadyFound(self, tab):
+        newTab=[]
 
-        name=self.writeUrlsToFile(listToSave)
-        print("All urls found are in the file {} !\n".format(name))
+        for url in tab:
+            if url not in self.urlsFound: #attention avec http vs https
+                newTab.append(url)
+        return newTab
+        
+    def getDomain(self, url):
+        return urlparse(url).netloc
 
-    def getMainDomain(self,url):
-        data=urlparse(url)
-        domain=data.netloc
-        tmp=domain.split(".")
-        return tmp[-2] + "." + tmp[-1]
+    def scan(self):
+        newUrls=[]
+        for url in self.urlsToScan:
+            print("SCANNING",url)
+            newUrls += self.scanUrl(url)
+        return newUrls
 
-    def printOtherDomains(self):
-        print("\nOTHER DOMAINS:")
-        for url in self.otherDomains:
-            if self.getMainDomain(url)==self.mainDomain:
-                print(url)
+    def scanUrl(self,url):
+        linksFound=[]
+        
+        hrefs=self.getHrefsUrl(url)
 
-    def printFoundUrls(self,listUrls):
-        print("\nUrls found ({}):".format(len(listUrls)))
-        listUrls.sort()
-        for url in listUrls:
-            print(url)
-        print("")
+        for u in hrefs:
+            link=u['href']
+            link=self.completeUrl(link)
+            link=self.cleanUrl(link)
 
-    def writeUrlsToFile(self,listUrls):
-        if self.outputFileName=="":
-            fileName=urlparse(self.url).netloc + ".txt"
-        else:
-            fileName=self.outputFileName
+            linksFound.append(link)
 
-        f = open(fileName, "w")
-        for url in listUrls:
-            f.write(url + "\n")
+        return linksFound
 
-        f.write("\n\n\nOTHER DOMAINS \n\n")
-
-        for url in self.otherDomains:
-            f.write(url + "\n")
-
-        f.close()
-
-        return fileName        
-
-    def removeSchemeUrl(self,url):
-        u=urlparse(url)
-        return u.netloc + u.path
-
-    def cleanUrl(self, url):
-        u=urlparse(url)
-        return u.scheme + "://" + u.netloc + u.path
-
-    def sameDomain(self, url):
-        return urlparse(url).netloc == (self.data).netloc
-    
-    def extract(self, url):
-        if self.isAFile(url): 
-            print("File",url)
-            return []
+    def getHrefsUrl(self,url):
         try:
             req = requests.get(url, self.headers, cookies={'PHPSESSID': self.cookie})
             soup = BeautifulSoup(req.content, 'html.parser')
-            tab=soup.find_all('a', href=True)
-
-            links=[]
-            for u in tab:
-                urlFound=u['href']
-                urlFound=self.completeUrl(urlFound)
-                urlFound=self.cleanUrl(urlFound)
-
-                if self.sameDomain(urlFound):
-                    links.append(urlFound)
-                else:
-                    (self.otherDomains).append(urlFound)
-
-            links= self.removeDuplicates(links)
-            return links
+            return soup.find_all('a', href=True)
         except:
-            print("\nERROR\n")
+            print("Error with",url)
             return []
-
-    def removeDuplicates(self,tab):
-        return list(dict.fromkeys(tab))
 
     def completeUrl(self,url):
         if urlparse(url).netloc=="":
             url = (self.data).scheme + "://" + (self.data).netloc + url
         return url
 
-    def isAFile(self,url):
-        url=urlparse(url).path
-        url=url.split("/")[-1]
-        if "." not in url: return False
+    def cleanUrl(self, url):
+        data=urlparse(url)
+        newUrl = data.scheme + "://" + data.netloc + data.path
 
-        url = url.split(".")[-1]
-        if url in self.allowedExtensions: return False
+        if newUrl[-1]=="/": 
+            newUrl=newUrl[:-1]
 
-        return True
+        return newUrl
 
     def stop(self, msg = ""):
         if msg != "": print(msg)
         exit(0)#stop the program
-
-    def help(self):
-        print("")
-        print("Usage: python weber.py -url URL [-deep x] [-v] [-o fileName] [-c cookie]")
-        print("")
-        print("Options:")
-        print("    -url URL        The url to scan")
-        print("    -deep x         Depth of scan, number of iteration (Optional, by default set to 2)")
-        print("    -v              Enable verbose during scan (Optional)")
-        print("    -o fileName     Output all urls found to this file (Optional, by default {domain}.txt)")
-        print("    -c cookie       Use the specified cookie (Optional)")
-        print("")
-        print("")
-        exit(0)
 
 if __name__ == '__main__':
     prog = program(sys.argv)
